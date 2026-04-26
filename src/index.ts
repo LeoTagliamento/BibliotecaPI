11cfbad46d7edce6dec07da37d7171a282f401f7
Leonardo Tagliamento <leonardotagliamento@hotmail.com>
2026-04-26 18:30:30 -0300

    feat: Loop function with retornarMenu, because while don't work

--- src/index.ts
+++ src/index.ts
@@ -18,15 +18,18 @@ const scanf = readline.createInterface({
 });
 
 
-    console.log("Tagliameto's Biblioteca");
-
+    console.log("\nTagliameto's Biblioteca");
+function retornarMenu(){
     scanf.question(
         "Escolha uma opção: \n 1 - Cadastrar livro \n 2 - Listar todos os livros \n 3 - Consultar livro \n 0 - Sair\n",
         (escolha) => {
     
             switch(escolha){
 
-                case '0': break;
+                case '0':
+                    console.log('Finalizando...');
+                    scanf.close();
+                    break;
 
                 case '1':
                     scanf.question("Digite o título do livro: ", (titulo: string) => {
@@ -38,7 +41,8 @@ const scanf = readline.createInterface({
                                             
                                             const novoLivro: Book = {titulo, autor, genero, ano:parseInt(ano), lido: lido === 'true', avaliacao:parseFloat(avaliacao)};
                                             biblioteca.push(novoLivro);
-
+                                            console.log('Livro cadastrado com sucesso.');
+                                            retornarMenu();
                                         });
                                     });
                                 });
@@ -48,18 +52,18 @@ const scanf = readline.createInterface({
                     break; 
 
                 case '2':  
-                console.log("Lista de livros cadastrados:");
-                   biblioteca.forEach((livro: Book, indice: number) => {
-                    console.log(`${indice + 1}. ${livro.titulo} - ${livro.autor} (${livro.ano}) - Gênero: ${livro.genero} - Lido: ${livro.lido} - Avaliação: ${livro.avaliacao}/5`);
-                });
-                break;
+                    console.log("Lista de livros cadastrados:");
+                    biblioteca.forEach((livro: Book, indice: number) => {
+                        console.log(`${indice + 1}. ${livro.titulo} - ${livro.autor} (${livro.ano}) - Gênero: ${livro.genero} - Lido: ${livro.lido} - Avaliação: ${livro.avaliacao}/5`);
+                    });
+                    retornarMenu();
+                    break;
 
                 case '3':
                     scanf.question("Tipo de consulta:\n 1 - Titulo \n 2 - Autor \n 3 - Gênero\n", (tipoConsulta: string) => {
                         switch(tipoConsulta){
                             case '1':
                                 scanf.question("Digite o título do livro que deseja consultar: ", (tituloConsulta: string) => {
-
                                     const resultados = biblioteca.filter((livro: Book) => livro.titulo.includes(tituloConsulta));
                                     if (resultados.length === 0) {
                                         console.log("Nenhum livro encontrado com esse título.");
@@ -68,36 +72,49 @@ const scanf = readline.createInterface({
                                             console.log(`${livro.titulo} - ${livro.autor} (${livro.ano}) - Gênero: ${livro.genero} - Lido: ${livro.lido} - Avaliação: ${livro.avaliacao}/5`);
                                         });
                                     }
+                                    retornarMenu();
                                 });
                                 break;
                             case '2':
                                 scanf.question("Digite o autor do livro: ", (autorConsulta: string) => {
                                     const resultados = biblioteca.filter((livro: Book) => livro.autor.includes(autorConsulta));
-                                     if (resultados.length === 0) {
+                                    if (resultados.length === 0) {
                                         console.log("Nenhum livro encontrado desse autor.");
                                     } else {
                                         resultados.forEach((livro) => {
                                             console.log(`${livro.titulo} - ${livro.autor} (${livro.ano}) - Gênero: ${livro.genero} - Lido: ${livro.lido} - Avaliação: ${livro.avaliacao}/5`);
                                         });
                                     }
+                                    retornarMenu();
                                 });
                                 break;
                             case '3':
                                 scanf.question("Digite o gênero do livro: ", (generoConsulta: string) => {
                                     const resultados = biblioteca.filter((livro: Book) => livro.genero.includes(generoConsulta));
-                                     if (resultados.length === 0) {
+                                    if (resultados.length === 0) {
                                         console.log("Nenhum livro encontrado desse gênero.");
                                     } else {
                                         resultados.forEach((livro) => {
                                             console.log(`${livro.titulo} - ${livro.autor} (${livro.ano}) - Gênero: ${livro.genero} - Lido: ${livro.lido} - Avaliação: ${livro.avaliacao}/5`);
                                         });
                                     }
+                                    retornarMenu();
                                 });
+                                break;
+                            default:
+                                console.log("Tipo de consulta inválido.");
+                                retornarMenu();
+                                break;
                         }
                     });
                     break;
 
-                    default: console.log("Opção inválida.");
+                default:
+                    console.log("Opção inválida.");
+                    retornarMenu();
+                    break;
             }
         }
     );
+}
+retornarMenu();
