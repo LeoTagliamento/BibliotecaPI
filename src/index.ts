5f2d5215c75f46a47aa6afac162c17b543d3867b
Leonardo Tagliamento <leonardotagliamento@hotmail.com>
2026-04-26 09:31:22 -0300

    feat: Interface de escolha

--- src/index.ts
+++ src/index.ts
@@ -9,14 +9,46 @@ type Book = {
     lido: boolean,
     avaliacao: number
 }
+
+const biblioteca: Book[] = [];
+
 const scanf = readline.createInterface({
     input: process.stdin,
     output: process.stdout  
-})
-escolha 
-while(escolha !== 0){
-console.log("Tagliameto's Biblioteca")
-console.log("Escolha uma opção: \n 1 - Cadastrar livro \n 2 - Listar todos os livros \n 3 - Consultar livro \n 0 - Sair"); 
-if escolha === 1 {
+});
 
+let escolha = 1;
+
+while(escolha !== 0){
+    console.log("Tagliameto's Biblioteca");
+
+    scanf.question(
+        "Escolha uma opção: \n 1 - Cadastrar livro \n 2 - Listar todos os livros \n 3 - Consultar livro \n 0 - Sair\n",
+        (escolha) => {
+    
+            switch(escolha){
+
+                case '0': break;
+
+                case '1':
+                    scanf.question("Digite o título do livro: ", (titulo: string) => {
+                        scanf.question("Digite o autor do livro: ", (autor: string) => {
+                            scanf.question("Digite o gênero do livro: ", (genero: string) => {
+                                scanf.question("Digite o ano de publicação do livro: ", (ano: number) => { 
+                                    scanf.question("O livro já foi lido? true/false: ", (lido: boolean) => {
+                                        scanf.question("Digite a avaliação do livro (0-5): ", (avaliacao: number) => {
+                                            
+                                            const novoLivro: Book = {titulo, autor, genero, ano, lido, avaliacao};
+                                            biblioteca.push(novoLivro);
+
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    });
+                    break;   
+            }
+        }
+    );
 }
