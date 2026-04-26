16b9c8f5aaacf5b9689918806806cd43ac38c137
Leonardo Tagliamento <leonardotagliamento@hotmail.com>
2026-04-26 08:48:29 -0300

    feat: interface + typagem

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,22 @@
 import * as fs from 'fs';
 import * as readline from 'readline';
+
+type Book = {
+    titulo: string,
+    autor: string,
+    genero: string,
+    ano: number,
+    lido: boolean,
+    avaliacao: number
+}
+const scanf = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout  
+})
+escolha 
+while(escolha !== 0){
+console.log("Tagliameto's Biblioteca")
+console.log("Escolha uma opção: \n 1 - Cadastrar livro \n 2 - Listar todos os livros \n 3 - Consultar livro \n 0 - Sair"); 
+if escolha === 1 {
+
+}
